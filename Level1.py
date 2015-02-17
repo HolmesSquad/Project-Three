9ca481458834493a3233eea5be1da823bdd78134
humphr36 <humphr36@users.noreply.github.com>
2015-02-17 16:58:45 +0000

    Update Level1.py

--- Level1.py
+++ Level1.py
@@ -1,99 +1,34 @@
 from Tkinter import *
+import time
 level1 = Tk()
 level1.title("Level 1")
 level1.resizable(0,0)
 canvas = Canvas(level1, width = 1280, height = 720, bg = "White")
 canvas.pack()
-
 level1Map = canvas.create_rectangle(20, 20, 1000, 700, fill = 'white', width = 2) 
-
 ListOfTreasures=[]
-
+ListOfRobots=[]
 NumberOfTreasures=0
-
+NumberOfRobots=0
 ProgramActive=False
-
 def callback(event):
     global NumberOfTreasures
     global NumberOfRobots
     if NumberOfTreasures<int(interface.MaxTreasures) and ProgramActive is False:
-        ListOfTreasures.append(treasure(event.x,event.y))
+        ListOfTreasures.append(treasures(event.x,event.y))
         NumberOfTreasures+=1
         print interface.MaxTreasures
         print NumberOfTreasures
         if NumberOfTreasures==int(interface.MaxTreasures):
-            interface.timerWindow()
-    '''elif NumberOfRobots<int(interface.MaxRobots) and ProgramActive is False:
+            interface.robotWindow()
+    elif NumberOfRobots<int(interface.MaxRobots) and ProgramActive is False:
         ListOfRobots.append(robots(event.x,event.y))
         NumberOfRobots+=1
         if NumberOfRobots==int(interface.MaxRobots):
             interface.startButton['state']='normal'
-            '''
             
 canvas.tag_bind(level1Map,"<Button-1>", callback)
 canvas.pack()
-
-'''class treasure:
-    global g
-    def __init__(self,x,y,x1,y1,colour,canvas,points):
-        self.x=x
-        self.y=y
-        self.x1=x1
-        self.y1=y1
-        self.colour=colour
-        self.canvas=canvas
-        self.points=points
-        self.object=canvas.create_rectangle(self.x,self.y,self.x1,self.y1,fill=self.colour)
-        #PointsList.append(points)
-
-    def bindLabel(self):
-        self.x, self.y, self.x1, self.y1 = canvas.coords(self.object)
-        canvas.tag_bind(self.object,"<Enter>", self.MouseRollover)
-        canvas.tag_bind(self.object, "<Leave>", self.MouseOff)
-
-    def MouseRollover(self, level1):
-        interface.squareLabel.place(x = self.x1 + 20, y = self.y - 20)
-        interface.whiteLabel.place(x = self.x1 + 20, y = self.y)
-        interface.points100Label.place(x = self.x1 + 20, y = self.y + 20)
-
-    def MouseOff(self, level1):
-        interface.squareLabel.place_forget()
-        interface.whiteLabel.place_forget()
-        interface.points100Label.place_forget()'''
-
-class treasure:
-    global canvas
-    global NumberOfTreasures
-    def __init__(self,x,y):
-        global NumberOfTreasures
-        self.type=interface.variable.get()
-        self.x=x
-        self.y=y
-        self.name="Treasure"+str(NumberOfTreasures)
-        if self.type=="Rectangle":
-            self.name=canvas.create_rectangle(self.x-10,self.y-10,self.x+10,self.y+10,fill='blue')
-            self.score=50
-        elif self.type=="Circle":
-            self.name=canvas.create_oval(self.x-10,self.y-10,self.x+10,self.y+10,fill='yellow')
-            self.score=75
-        elif self.type=="Triangle":
-            self.name=canvas.create_polygon(self.x,self.y-20,self.x-10,self.y,self.x+10,self.y,fill='green')
-            self.score=100
-
-        def bindLabel(self):
-            self.x, self.y = canvas.coords(self.name)
-            canvas.tag_bind(self.name,"<Enter>", self.MouseRolloverSquare)
-            canvas.tag_bind(self.name, "<Leave>", self.MouseOffSquare)
-
-        def MouseRolloverSquare(self, level1):
-            interface.squareLabel.place(x = self.x + 30, y = self.y - 20)
-            interface.whiteLabel.place(x = self.x + 30, y = self.y)
-            interface.points100Label.place(x = self.x + 30, y = self.y + 20)
-
-        def MouseOffSquare(self, level1):
-            interface.squareLabel.place_forget()
-            interface.whiteLabel.place_forget()
-            interface.points100Label.place_forget()
 
 class interface:
     def __init__(self, name):
@@ -109,13 +44,13 @@ class interface:
         self.secShowLabel = Label(name, text = "00", width = 5, height = 2, font = ("Arial", 16), bg = "Gray")
         self.secShowLabel.place(x = 1200, y = 20)
 
-        self.startButton = Button(name, text = "Start", width = 20, command = self.start, font = ("Arial", 16),bg = "LightGreen")
+        self.startButton = Button(name, text = "Start", width = 20, command = self.start, font = ("Arial", 16),bg = "LightGreen",state=DISABLED)
         self.startButton.place(x = 1020, y = 80)
 
         self.resetButton = Button(name, text = "Reset", width = 20, command = '', font = ("Arial", 16), bg = "Orange")
         self.resetButton.place(x = 1020, y = 130)
 
-        self.pauseButton = Button(name, text = "Pause", width = 20, command = '', font = ("Arial", 16), bg = "Yellow")
+        self.pauseButton = Button(name, text = "Pause", width = 20, command = '', font = ("Arial", 16), bg = "Yellow",state=DISABLED)
         self.pauseButton.place(x = 1020, y = 180)
 
         self.levelSelectButton = Button(name, text = "Level Select", width = 20, command = self.levelSelect, font = ("Arial", 16), bg = "LightBlue")
@@ -127,16 +62,13 @@ class interface:
         self.scoreShowLabel = Label(name, text = "000", width = 10, height = 2, font = ("Arial", 16), bg = "LightGray")
         self.scoreShowLabel.place(x = 1140, y = 290)
 
-        self.treasureCollectedLabel = Label(name, text = "Treasure Collected", width = 20, height = 1, font = ("Arial", 16), bg = "LightGray")
+        self.treasureCollectedLabel = Label(name, text = "Robot 1 Treasure Collected", width = 22, height = 1, font = ("Arial", 14), bg = "LightGray")
         self.treasureCollectedLabel.place(x = 1020, y = 350)
+        self.treasureCollectedLabel2 = Label(name, text = "Robot 2 Treasure Collected", width = 22, height = 1, font = ("Arial", 14), bg = "LightGray")
+        self.treasureCollectedLabel2.place(x = 1020, y = 450)
 
-        self.treasureBackgroundLabel = Label(name, width = 20, height = 8, font = ("Arial", 16), bg = "LightGray")
-        self.treasureBackgroundLabel.place(x = 1020, y = 380)
-
-        self.squareLabel = Label(name, text = "Shape = Square  ", bg = "White", font = ("Arial", 10))
-        self.whiteLabel = Label(name, text = "Colour = White     ", bg = "White", font = ("Arial", 10))
-        self.points100Label = Label(name, text = "Worth = 100 Points ", bg = "White", font = ("Arial", 10))
-
+        
+        #Create DropDown List for selecting which type of treasure to create
         self.OPTIONS = [
             "Rectangle",
             "Circle",
@@ -158,25 +90,6 @@ class interface:
             level1.secShowLabel.after(1000, level1.timer)
          else:
             level1.counter_stop()
-
-    def treasureWindow(level1):
-        global treasureWindow
-        treasureWindow = Tk()
-        treasureWindow.title("Number of Treasures")
-        treasureWindow.resizable(0,0)
-        
-        treasureCanvas = Canvas(treasureWindow, width = 210, height = 200, bg = "White")
-        
-        interface.treasure_label = Label(treasureCanvas, text = "Number of Treasures (Max:10)", wraplength = 100, width = 20, font = ("Arial", 9), bg = "White")
-        interface.treasure_label.place(x = 35, y = 10)
-        
-        interface.treasureEntry = Entry(treasureCanvas, text= "" , width = 20, bd = 5)
-        interface.treasureEntry.place(x = 45,y = 60)
-        
-        interface.treasureEntryButton = Button(treasureCanvas, text="Ok", width = 10, font = ("Arial", 10),command=interface.assignmaxtreasures, bg = "LightGreen")
-        interface.treasureEntryButton.place(x = 65, y = 100)
-        
-        treasureCanvas.pack()
 
     def timerShow(level1,self):
         global counter, RoboFinished
@@ -205,17 +118,65 @@ class interface:
         interface.timeEntryButton.place(x = 65, y = 100)
         
         timerCanvas.pack()
+
+    def treasureWindow(level1):
+        global treasureWindow
+        treasureWindow = Tk()
+        treasureWindow.title("Number of Treasures")
+        treasureWindow.resizable(0,0)
+        
+        treasureCanvas = Canvas(treasureWindow, width = 210, height = 200, bg = "White")
+        
+        interface.treasure_label = Label(treasureCanvas, text = "Number of Treasures (Max:10)", wraplength = 100, width = 20, font = ("Arial", 9), bg = "White")
+        interface.treasure_label.place(x = 35, y = 10)
+        
+        interface.treasureEntry = Entry(treasureCanvas, text= "" , width = 20, bd = 5)
+        interface.treasureEntry.place(x = 45,y = 60)
+        
+        interface.treasureEntryButton = Button(treasureCanvas, text="Ok", width = 10, font = ("Arial", 10),command=interface.assignmaxtreasures, bg = "LightGreen")
+        interface.treasureEntryButton.place(x = 65, y = 100)
+        
+        treasureCanvas.pack()
+        
     def assignmaxtreasures(self):
+        global TreasuresRemaining
         if int(interface.treasureEntry.get())>10:
             print "No more than ten treasures can be created"
         else:
             self.MaxTreasures=interface.treasureEntry.get()
+            TreasuresRemaining=int(self.MaxTreasures)
             treasureWindow.destroy()
 
-    def start(self):
-        self.treasureWindow()
-        interface.startButton.place_forget()
+    def robotWindow(level1):
+        global robotWindow
+        robotWindow = Tk()
+        robotWindow.title("Number of Robots")
+        robotWindow.resizable(0,0)
         
+        robotCanvas = Canvas(robotWindow, width = 210, height = 200, bg = "White")
+        
+        interface.robot_label = Label(robotCanvas, text = "Number of Robots (Max: 2)", wraplength = 100, width = 20, font = ("Arial", 9), bg = "White")
+        interface.robot_label.place(x = 35, y = 10)
+        
+        interface.robotEntry = Entry(robotCanvas, text= "" , width = 20, bd = 5)
+        interface.robotEntry.place(x = 45,y = 60)
+        
+        interface.robotEntryButton = Button(robotCanvas, text="Ok", width = 10, font = ("Arial", 10),command=interface.assignmaxrobots, bg = "LightGreen")
+        interface.robotEntryButton.place(x = 65, y = 100)
+        
+        robotCanvas.pack()
+        
+    def assignmaxrobots(self):
+        if int(interface.robotEntry.get())>2:
+            print "No more than two robots can be created"
+        else:
+            self.MaxRobots=interface.robotEntry.get()
+            robotWindow.destroy()
+        
+    def start(self):
+        self.timerWindow()
+        interface.startButton.place_forget()
+
     def timerwinget(level1):
         global counter, timerWindow
         if (interface.timeEntry.get())=="":
@@ -224,11 +185,19 @@ class interface:
             counter=int(interface.timeEntry.get())
         interface.timerShow(interface)
         timerWindow.destroy()
+        ListOfRobots[0].TreasuresFoundPositions=[[1025,390,1045,410],[1055,390,1075,410],[1085,390,1105,410],[1115,390,1145,410],[1145,390,1165,410],[1175,390,1195,410],[1205,390,1225,410],[1235,390,1255,410],[1025,420,1045,440],[1055,420,1075,440]]
+        ListOfRobots[1].TreasuresFoundPositions=[[1025,490,1045,510],[1055,490,1075,510],[1085,490,1105,510],[1115,490,1145,510],[1145,490,1165,510],[1175,490,1195,510],[1205,490,1225,510],[1235,490,1255,510],[1025,520,1045,540],[1055,520,1075,540]]
+        interface.pauseButton['state']='normal'
+        for robot in ListOfRobots:
+            robot.closesttreasure()
+            robot.moveto(robot.ClosestTreasure.x,robot.ClosestTreasure.y)
+        while TreasuresRemaining>0:
+            for robot in ListOfRobots:
+                robot.move()
         
     def reset(self):
         print "Reset"
-        print event.x
-        print event.y
+
     def pause(self):
         print "Pause"
 
@@ -271,10 +240,133 @@ class interface:
         level1.destroy()
         import Level3
 
-for i in ListOfTreasures:
-    i = interface(event.x, enent.y)
-    print event.x
-    print event.y
-interface = interface(level1)
+class treasures:
+    global canvas
+    global NumberOfTreasures
+    def __init__(self,x,y):
+        global NumberOfTreasures
+        self.type=interface.variable.get()
+        self.x=x
+        self.y=y
+        self.name="Treasure"+str(NumberOfTreasures)
+        self.found=False
+        if self.type=="Rectangle":
+            self.name=canvas.create_rectangle(self.x-10,self.y-10,self.x+10,self.y+10,fill='blue')
+            self.score=50
+            self.colour='Blue'
+        elif self.type=="Circle":
+            self.name=canvas.create_oval(self.x-10,self.y-10,self.x+10,self.y+10,fill='yellow')
+            self.score=75
+            self.colour='Yellow'
+        elif self.type=="Triangle":
+            self.name=canvas.create_polygon(self.x,self.y-10,self.x-10,self.y+10,self.x+10,self.y+10,fill='green')
+            self.score=100
+            self.colour='Green'
+        self.ShapeLabel = Label(level1, text = "Shape = "+self.type, bg = "White", font = ("Arial", 10))
+        self.ColourLabel = Label(level1, text = "Colour = "+self.colour, bg = "White", font = ("Arial", 10))
+        self.WorthLabel = Label(level1, text = "Worth = "+str(self.score), bg = "White", font = ("Arial", 10))
+        canvas.tag_bind(self.name,"<Enter>", self.MouseRollover)
+        canvas.tag_bind(self.name, "<Leave>", self.MouseOff)
+    def MouseRollover(self,level1):
+        self.ShapeLabel.place(x = self.x + 20, y = self.y - 20)
+        self.ColourLabel.place(x = self.x + 20, y = self.y)
+        self.WorthLabel.place(x = self.x + 20, y = self.y+20)
 
+    def MouseOff(self,level1):
+        self.ShapeLabel.place_forget()
+        self.ColourLabel.place_forget()
+        self.WorthLabel.place_forget()
+class robots:
+    def __init__(self,x,y):
+        self.x1=x-10
+        self.y1=y-10
+        self.x2=x+10
+        self.y2=y+10
+        self.x=x
+        self.y=y
+        self.speed=1
+        self.canvas=canvas
+        self.shape=canvas.create_rectangle(self.x-10,self.y-10,self.x+10,self.y+10,fill='cyan')
+        self.TreasuresFound=[]
+        self.TreasuresFoundPositions=[]
+        self.NumberOfTreasuresFound=0
+    def closesttreasure(self):
+        lowestdistance=100000
+        for i in ListOfTreasures:
+            if i.found==False:
+                if i.x>self.x:
+                    xdistance=i.x-self.x
+                elif i.x<self.x:
+                    xdistance=self.x-i.x
+                else:
+                    xdistance=0
+                if i.y>self.y:
+                    ydistance=i.y-self.y
+                elif i.y<self.y:
+                    ydistance=self.y-i.y
+                else:
+                    ydistance=0
+                totaldistance=(ydistance**2+xdistance**2)**0.5
+                if totaldistance<lowestdistance:
+                    lowestdistance=totaldistance
+                    self.ClosestTreasure=i
+    def moveto(self,xdest,ydest):
+        if xdest>self.x:
+            xdistance=xdest-self.x
+        elif xdest<self.x:
+            xdistance=self.x-xdest
+        else:
+            xdistance=0
+        if ydest>self.y:
+            ydistance=ydest-self.y
+        elif ydest<self.y:
+            ydistance=self.y-ydest
+        else:
+            ydistance=0
+        totaldistance=(ydistance**2+xdistance**2)**0.5
+        if xdest>self.x:
+            self.vx=(xdistance/totaldistance)*self.speed
+        elif xdest<self.x:
+            self.vx=(0-(xdistance/totaldistance))*self.speed
+        else:
+            self.vx=0
+        if ydest>self.y:
+            self.vy=(ydistance/totaldistance)*self.speed
+        elif ydest<self.y:
+            self.vy=(0-(ydistance/totaldistance))*self.speed
+        else:
+            self.vy=0
+        print str(totaldistance)
+        self.distanceleft=int(totaldistance)
+    def move(self):
+        global TreasuresRemaining
+        if self.distanceleft>0 and self.ClosestTreasure.found==False:
+            self.x1+=self.vx
+            self.x2+=self.vx
+            self.y1+=self.vy
+            self.y2+=self.vy
+            self.x+=self.vx
+            self.y+=self.vy
+            self.canvas.coords(self.shape,self.x1,self.y1,self.x2,self.y2)
+            self.canvas.update()
+            self.distanceleft-=1
+            time.sleep(0.01)
+        else:
+            if self.ClosestTreasure.found==False:
+                self.ClosestTreasure.found=True
+                self.TreasuresFound.append(self.ClosestTreasure)
+                self.canvas.coords(self.ClosestTreasure.name,self.TreasuresFoundPositions[self.NumberOfTreasuresFound][0],self.TreasuresFoundPositions[self.NumberOfTreasuresFound][1],self.TreasuresFoundPositions[self.NumberOfTreasuresFound][2],self.TreasuresFoundPositions[self.NumberOfTreasuresFound][3])
+                self.canvas.update()
+                TreasuresRemaining-=1
+                self.NumberOfTreasuresFound+=1
+            if TreasuresRemaining>0:
+                self.closesttreasure()
+                self.moveto(self.ClosestTreasure.x,self.ClosestTreasure.y)
+            else:
+                self.vx=0
+                self.vy=0
+interface = interface(level1)
+interface.MaxTreasures=0
+interface.MaxRobots=0
+interface.treasureWindow()
 level1.mainloop()    
